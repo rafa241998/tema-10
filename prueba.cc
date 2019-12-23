235208143254468cea6402098a3be8c9ff051c9f
Rafael Tomás <rts19@alu.ua.es>
2019-12-23 12:21:30 +0100

    Calculado el día de la muerte

--- prueba.cc
+++ prueba.cc
@@ -7,12 +7,12 @@ int main(){
     int year;
     int month;
     int day;
-    cout << "Este es mi programa inicial:" << endl;
+    cout << "Este es mi programa inicial." << endl;
     cout << "Introduce tu fecha de nacimiento con el formato yyyy-mm-dd:" << endl;
     cin >> s;
     year = stoi(s.substr(0,4));
     month = stoi(s.substr(5,7));
     day = stoi(s.substr(8));
-    cout << "El día de muerte será:"  << endl;
-
+    cout << "La fecha de tu de muerte será:"  << endl;
+    cout << year+20<< "-" <<month<<"-"<<day<<endl;
 }
