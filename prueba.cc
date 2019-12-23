1d8bd3b995965ebea19f0a121456bea07a931865
Rafael Tomás <rts19@alu.ua.es>
2019-12-23 12:18:59 +0100

    Leer de consola una fecha y dividirla

---
+++ prueba.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string s;
+    int year;
+    int month;
+    int day;
+    cout << "Este es mi programa inicial:" << endl;
+    cout << "Introduce tu fecha de nacimiento con el formato yyyy-mm-dd:" << endl;
+    cin >> s;
+    year = stoi(s.substr(0,4));
+    month = stoi(s.substr(5,7));
+    day = stoi(s.substr(8));
+    cout << "El día de muerte será:"  << endl;
+
+}
