2cedff6353f93d3044c93b8edefd4a2f547b4d89
Rafael Tomás <rts19@alu.ua.es>
2019-12-23 12:31:39 +0100

    Calculado el día de los muertos a partir de tu muerte

--- prueba.cc
+++ prueba.cc
@@ -7,7 +7,7 @@ int main(){
     int year;
     int month;
     int day;
-    cout << "Este es mi programa inicial." << endl;
+    cout << "Este es mi programa inicial" << endl;
     cout << "Introduce tu fecha de nacimiento con el formato yyyy-mm-dd:" << endl;
     cin >> s;
     year = stoi(s.substr(0,4));
@@ -15,4 +15,19 @@ int main(){
     day = stoi(s.substr(8));
     cout << "La fecha de tu de muerte será:"  << endl;
     cout << year+20<< "-" <<month<<"-"<<day<<endl;
+    cout << "El dia de los muertos para que tu familia vaya a visitarte es:" << endl;
+    if(month>11){
+        cout << year+21 <<"-11-2"<< endl;
+    }else{
+        if(month==11){
+            if(day>2){
+                cout << year+21 <<"-11-2"<< endl;
+            }else{
+                cout << year+20 <<"-11-2"<< endl;
+            }
+        }else{
+            cout << year+20 <<"-11-2"<< endl;
+        }
+    }
+
 }
